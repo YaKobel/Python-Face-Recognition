ae768f2e3a548d39c68199594cb19f471a6ec6f9
Yaroslav Kobel <yarokobel@gmail.com>
2020-06-04 12:22:17 +0300

    Find Elon Musk

--- indentify.py
+++ indentify.py
@@ -7,19 +7,24 @@ bill_face_encoding = face_recognition.face_encodings(image_of_bill)[0]
 image_of_steve = face_recognition.load_image_file('./img/known/Steve Jobs.jpg')
 steve_face_encoding = face_recognition.face_encodings(image_of_steve)[0]
 
+image_of_musk = face_recognition.load_image_file('./img/known/Elon Musk.jpg')
+musk_face_encoding = face_recognition.face_encodings(image_of_musk)[0]
+
 # Create array of encoding and names
 known_face_encodings = [
     bill_face_encoding,
-    steve_face_encoding
+    steve_face_encoding,
+    musk_face_encoding
 ]
 
 known_face_names = [
     "Bill Gates",
-    "Steve Jobs"
+    "Steve Jobs",
+    "Elon Musk"
 ]
 
 # Load test image to find faces in
-test_image = face_recognition.load_image_file('./img/groups/bill-steve.jpg')
+test_image = face_recognition.load_image_file('./img/groups/bill-steve-elon.jpg')
 
 # Find faces in test image
 face_locations = face_recognition.face_locations(test_image)
@@ -43,12 +48,12 @@ for(top, right, bottom, left), face_encoding in zip(face_locations, face_encodin
         name = known_face_names[first_match_index]
 
     # Draw Box
-    draw.rectangle(((left, top),(right, bottom)), outline=(0,0,0))
+    draw.rectangle(((left, top),(right, bottom)), outline=(255,255,0))
 
     # Draw Label
     text_width, text_height = draw.textsize(name)
-    draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 0), outline=(0, 0, 0))
-    draw.text((left + 6, bottom - text_height - 5), name, fill=(255,255,255,255))
+    draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(255, 255, 0), outline=(255, 255, 0))
+    draw.text((left + 6, bottom - text_height - 5), name, fill=(0,0,0))
 
 del draw
 
